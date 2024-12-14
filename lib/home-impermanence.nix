b11237f68bc9c3061ed61a9023d2fff1f97c84a0
unlsycn <unlsycn@unlsycn.com>
2024-12-14 20:20:34 +0800

    fix: use valueType instead of anything as type of persist
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- lib/home-impermanence.nix
+++ lib/home-impermanence.nix
@@ -3,5 +3,21 @@ with lib;
 {
   # workaround for https://github.com/nix-community/impermanence/issues/231
   # To keep the impermanence config together with the other configs for better cohesion, we define this option and merge it into nixosConfiguration in ../system/commom/impermanence.nix
-  options.persist = mkOption { type = types.anything; };
+  options.persist = mkOption {
+    type =
+      with types;
+      let
+        # use valueType instead of anything to merge lists correctly
+        valueType = nullOr (oneOf [
+          bool
+          int
+          float
+          str
+          path
+          (attrsOf valueType)
+          (listOf valueType)
+        ]);
+      in
+      valueType;
+  };
 }
