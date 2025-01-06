42a5913e9b368f2bac62cac749e760a770dce880
unlsycn <unlsycn@unlsycn.com>
2025-01-06 11:31:32 +0800

    fix: restart sops service on failure since it require passphrase
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/sops/default.nix
+++ home/modules/sops/default.nix
@@ -1,6 +1,16 @@
-{ config, user, ... }:
+{
+  config,
+  user,
+  ...
+}:
 {
   sops.age.keyFile = "${config.xdg.configHome}/age/key";
+
+  # sops require gnupg passphrase
+  systemd.user.services.sops-nix.Service = {
+    Restart = "on-failure";
+    RestartSec = "5s";
+  };
 
   persist."/persist".users.${user}.files = [ ".config/age/key" ];
 }
