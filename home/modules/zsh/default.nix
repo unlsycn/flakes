2ac51538cd707ff64ee1f69a2f06a316e1a16f54
unlsycn <unlsycn@unlsycn.com>
2025-03-05 10:36:10 +0800

    fix(zsh): autoload compinit before use
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -105,6 +105,7 @@ in
       '';
 
       initExtra = ''
+        autoload -Uz compinit
         compinit -d ~/.cache/zcompdump
         zstyle ':completion:*:*:*:*:*' menu select
         zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' # case insensitive tab completion
