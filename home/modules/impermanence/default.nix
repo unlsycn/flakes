d2365171ce11bb6a2ab4608289226fd5fe35181e
unlsycn <unlsycn@unlsycn.com>
2025-04-06 20:40:18 +0800

    feat(impermanaence): use as home module
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/impermanence/default.nix
+++ home/modules/impermanence/default.nix
@@ -2,7 +2,7 @@
 with lib;
 {
   # workaround for https://github.com/nix-community/impermanence/issues/231
-  # To keep the impermanence config together with the other configs for better cohesion, we define this option and merge it into nixosConfiguration in ../system/commom/impermanence.nix
+  # To keep the impermanence config together with the other configs for better cohesion, we define this option and merge it into nixosConfiguration in system/commom/impermanence.nix
   options.persist = mkOption {
     type =
       with types;
--- system/common/impermanence.nix
+++ system/common/impermanence.nix
@@ -9,7 +9,7 @@ let
   homeConfig = config.home-manager.users.${user};
 in
 {
-  # See ../../lib/home-impermanence.nix
+  # See home/modules/impermanence/default.nix
   # use mkIf for lazy evaluation
   config = mkIf (builtins.hasAttr "persist" homeConfig) {
     environment.persistence = homeConfig.persist;
