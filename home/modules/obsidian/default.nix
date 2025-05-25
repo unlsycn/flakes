02852b0b60eb8e8e7b40929252d39d2e49313f5e
unlsycn <unlsycn@unlsycn.com>
2025-05-25 10:48:04 +0800

    fix(obsidian): use obsidian module in home-manager
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/obsidian/default.nix
+++ home/modules/obsidian/default.nix
@@ -12,15 +12,7 @@ let
   better-sqlite3 = pkgs.callPackage ./better-sqlite3.nix { };
 in
 {
-  options.programs.obsidian = {
-    enable = mkEnableOption "Powerful knowledge base that works on top of a local folder of plain text Markdown files";
-    package = mkPackageOption pkgs "Obsidian" {
-      default = [ "obsidian" ];
-    };
-  };
-
   config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
     xdg.dataFile."Zotero/better_sqlite3.node".source = better-sqlite3;
   };
 }
