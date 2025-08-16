f40b2bf6cd6661c0585ba31f17801a21b97bf21f
unlsycn <unlsycn@unlsycn.com>
2025-08-16 19:11:17 +0800

    fix(workarounds): declare root gitconfig in nix
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- system/modules/workarounds/default.nix
+++ system/modules/workarounds/default.nix
@@ -1,7 +1,20 @@
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   # workaround for https://github.com/NixOS/nix/issues/10202
-  environment.persistence."/persist".files = [ "/root/.gitconfig" ];
+  systemd.tmpfiles.settings."root-gitconfig"."/root/.gitconfig".L = {
+    user = "root";
+    group = "root";
+    argument = "${pkgs.writeText "root-gitconfig" ''
+      [safe]
+        directory = /home/unlsycn/.nix
+        directory = /home/unlsycn/.cache/nix/tarball-cache
+    ''}";
+  };
 
   # workaround for https://github.com/NixOS/nixpkgs/issues/6481
   systemd.tmpfiles.rules = lib.concatLists (
