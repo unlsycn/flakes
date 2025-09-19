ecc58341cdd847ba479c3691408530d7883a026c
unlsycn <unlsycn@unlsycn.com>
2025-09-19 18:53:11 +0800

    fix(allay): add support for system-wide pipewire
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- system/hosts/x86_64-linux/allay/sound.nix
+++ system/hosts/x86_64-linux/allay/sound.nix
@@ -1,19 +1,30 @@
 {
+  config,
   pkgs,
+  lib,
   ...
 }:
+with lib;
 let
+  systemWide = config.services.pipewire.systemWide;
   ALSA_CONFIG_UCM2 = "/run/current-system/sw/share/alsa/ucm2";
 in
 {
-  environment.systemPackages = with pkgs; [
-    lnl-alsa-ucm-conf
-  ];
-  environment.pathsToLink = [
-    "/share/alsa"
-  ];
-  systemd.user.services.pipewire.environment.ALSA_CONFIG_UCM2 = ALSA_CONFIG_UCM2;
-  systemd.user.services.wireplumber.environment.ALSA_CONFIG_UCM2 = ALSA_CONFIG_UCM2;
+  environment = {
+    systemPackages = with pkgs; [
+      lnl-alsa-ucm-conf
+    ];
+    pathsToLink = [
+      "/share/alsa"
+    ];
+  };
+
+  systemd = {
+    services.pipewire.environment.ALSA_CONFIG_UCM2 = mkIf systemWide ALSA_CONFIG_UCM2;
+    services.wireplumber.environment.ALSA_CONFIG_UCM2 = mkIf systemWide ALSA_CONFIG_UCM2;
+    user.services.pipewire.environment.ALSA_CONFIG_UCM2 = mkIf (!systemWide) ALSA_CONFIG_UCM2;
+    user.services.wireplumber.environment.ALSA_CONFIG_UCM2 = mkIf (!systemWide) ALSA_CONFIG_UCM2;
+  };
 
   hardware.firmware = [
     pkgs.sof-firmware
