a4a1024bccaa46a74078f1d06c071b567f846bee
unlsycn <unlsycn@unlsycn.com>
2025-10-09 00:28:41 +0800

    fix(vscode): add continue support
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/vscode/default.nix
+++ home/modules/vscode/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   user,
   ...
 }:
@@ -8,12 +9,31 @@ with lib;
 {
   imports = [ ./hyprland.nix ];
 
+  options.programs.vscode.continue = {
+    enable = mkEnableOption "Continue extension for VSCode";
+  };
+
   config = mkIf config.programs.vscode.enable {
     persist."/persist".users.${user}.directories = [
       ".config/Code"
       ".vscode"
-    ];
+    ]
+    ++ optional config.programs.vscode.continue.enable ".continue";
+
+    programs.vscode.package =
+      if config.programs.vscode.continue.enable then
+        # https://github.com/continuedev/continue/issues/821#issuecomment-3227673526
+        (pkgs.vscode.overrideAttrs (
+          final: prev: {
+            preFixup =
+              prev.preFixup
+              + "gappsWrapperArgs+=( --prefix LD_LIBRARY_PATH : ${makeLibraryPath [ pkgs.gcc.cc.lib ]} )";
+          }
+        ))
+      else
+        pkgs.vscode;
 
     services.wakatime.enable = true;
+    programs.vscode.continue.enable = true;
   };
 }
