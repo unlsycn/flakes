0a31f778928927e4b2435f5761a9e301e1bf89db
unlsycn <unlsycn@unlsycn.com>
2025-12-07 16:03:17 +0800

    build: update flake
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- flake.nix
+++ flake.nix
@@ -25,6 +25,10 @@
       url = "github:unlsycn/Jovian-NixOS";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    foundryvtt = {
+      url = "github:reckenrode/nix-foundryvtt";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
