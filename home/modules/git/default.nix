e8685a157eb3681b20aa3c8896a47c85fe6e7418
unlsycn <unlsycn@unlsycn.com>
2025-12-07 16:56:39 +0800

    chore: update git options
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/git/default.nix
+++ home/modules/git/default.nix
@@ -9,38 +9,38 @@ let
 in
 {
   config = lib.mkIf config.programs.git.enable {
-    programs.git = {
-      userEmail = "unlsycn@unlsycn.com";
-      userName = "unlsycn";
+    programs = {
+      git = {
+        settings = {
+          user.email = "unlsycn@unlsycn.com";
+          user.name = "unlsycn";
 
+          init.defaultBranch = "main";
+          core.editor = "${nvim}";
+          merge.conflictStyle = "diff3";
+          log.decorate = "auto";
+
+          alias = {
+            amend = "commit --amend";
+            fixup = "!f(){ git reset --soft HEAD~\${1} && git commit --amend -C HEAD; };f";
+            cm = "commit -m";
+            cl = "git commit -c ORIG_HEAD";
+            spu = "stash push";
+            spo = "stash pop";
+            lg = "log --oneline --graph --all";
+          };
+        };
+        signing = {
+          signByDefault = true;
+          key = null;
+        };
+      };
       delta = {
         enable = true;
         options = {
           navigate = true;
           light = false;
         };
-      };
-
-      signing = {
-        signByDefault = true;
-        key = null;
-      };
-
-      aliases = {
-        amend = "commit --amend";
-        fixup = "!f(){ git reset --soft HEAD~\${1} && git commit --amend -C HEAD; };f";
-        cm = "commit -m";
-        cl = "git commit -c ORIG_HEAD";
-        spu = "stash push";
-        spo = "stash pop";
-        lg = "log --oneline --graph --all";
-      };
-
-      extraConfig = {
-        init.defaultBranch = "main";
-        core.editor = "${nvim}";
-        merge.conflictStyle = "diff3";
-        log.decorate = "auto";
       };
     };
   };
