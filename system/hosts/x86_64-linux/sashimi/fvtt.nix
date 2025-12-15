37aa962116d526bd7ac59b552c46ce41d4f007d6
unlsycn <unlsycn@unlsycn.com>
2025-12-15 13:31:58 +0800

    fix(fvtt): update configs
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- system/hosts/x86_64-linux/sashimi/fvtt.nix
+++ system/hosts/x86_64-linux/sashimi/fvtt.nix
@@ -1,12 +1,27 @@
-{ inputs', ... }:
 {
-  services.foundryvtt = {
-    enable = true;
-    package = inputs'.foundryvtt.packages.foundryvtt_13;
-    hostName = "fvtt.unlsycn.com";
-    port = 1501;
-    minifyStaticFiles = true;
-    upnp = false;
-    language = "cn.foundry_chn";
+  inputs',
+  lib,
+  ...
+}:
+with lib;
+{
+  options.services.foundryvtt = {
+    telemetry = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = {
+    services.foundryvtt = {
+      enable = true;
+      package = inputs'.foundryvtt.packages.foundryvtt_13;
+      hostName = "fvtt.unlsycn.com";
+      port = 1501;
+      minifyStaticFiles = true;
+      upnp = false;
+      language = "cn.foundry_chn";
+      telemetry = false;
+    };
   };
 }
