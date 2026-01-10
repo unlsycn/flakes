fcd8a87ee0d90f26ea4ec730f1ecc0a19c50e4bc
unlsycn <unlsycn@unlsycn.com>
2026-01-10 17:08:59 +0800

    feat(wireguard): add home peer
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- system/hosts/x86_64-linux/lonicera/wireguard.nix
+++ system/hosts/x86_64-linux/lonicera/wireguard.nix
@@ -8,15 +8,22 @@
         privateKeyFile = config.sops.secrets.wg0.path;
         peers = [
           {
+            # gabriel
+            publicKey = "ALRmt/glFRLSHDhN14y0wo4NsrbS09yl9Unh8h3sjkw=";
+            allowedIPs = [ "10.122.133.11/32" ];
+          }
+          {
             # allay
             publicKey = "ZMGy0DibJp7pvC8E4OGXhmUuEXpiNwinx4sPxZ1TUCU=";
             allowedIPs = [ "10.122.133.72/32" ];
           }
           {
+            # artanis
             publicKey = "TNXQNQSFMs0XKWpXpjVBIoSCYRcbPie7KnmMZ47vC1U=";
             allowedIPs = [ "10.122.133.73/32" ];
           }
           {
+            # max
             publicKey = "mkGpgHDEQVCAKTaYCZCsa8PoqmQ00UNX801PrG+nQnc=";
             allowedIPs = [ "10.122.133.41/32" ];
           }
