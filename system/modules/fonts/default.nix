c0f64effc9959d0c7ed74430fe73d1ee45e1273f
unlsycn <unlsycn@unlsycn.com>
2026-01-26 02:25:33 +0800

    feat(fonts): add fontconfig
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- system/modules/fonts/default.nix
+++ system/modules/fonts/default.nix
@@ -7,22 +7,45 @@
 with lib;
 {
   config = lib.mkIf config.hasDesktopEnvironment {
-    fonts.packages = with pkgs; [
-      maple-mono.truetype
-      maple-mono.NF-unhinted
-      jetbrains-mono
+    fonts = {
+      packages = with pkgs; [
+        maple-mono.truetype
+        maple-mono.NF-unhinted
+        jetbrains-mono
 
-      inter
-      roboto
-      merriweather
+        inter
+        roboto
+        merriweather
 
-      lxgw-wenkai
-      sarasa-gothic
+        lxgw-wenkai
+        sarasa-gothic
 
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.roboto-mono
+        nerd-fonts.jetbrains-mono
+        nerd-fonts.roboto-mono
 
-      openmoji-color
-    ];
+        openmoji-color
+      ];
+
+      fontconfig = {
+        enable = true;
+        defaultFonts = {
+          sansSerif = [
+            "Inter"
+            "Sarasa Mono SC"
+            "Roboto"
+          ];
+
+          serif = [
+            "Merriweather"
+            "LXGW WenKai Mono"
+          ];
+
+          monospace = [
+            "Maple Mono NF"
+            "Sarasa Mono SC"
+          ];
+        };
+      };
+    };
   };
 }
