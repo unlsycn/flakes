e6631ec15c9abacf5cf671ac8f2694de093dfd4a
unlsycn <unlsycn@unlsycn.com>
2026-02-14 17:27:23 +0800

    fix(hyprland): update HDMI monitor refresh rate to 144Hz
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/hyprland/monitors.nix
+++ home/modules/hyprland/monitors.nix
@@ -19,7 +19,7 @@ in
     wayland.windowManager.hyprland.settings.monitor = [
       (forMonitor "allay" "eDP-1, 2880x1800@120, 0x0, 1.8")
       (forMonitor "philips" "DP-1, 2560x1440@165, 0x-1152, 1.25")
-      (forMonitor "philips" "HDMI-A-1, 2560x1440@120, 0x-1152, 1.25")
+      (forMonitor "philips" "HDMI-A-1, 2560x1440@144, 0x-1152, 1.25")
     ];
   };
 }
