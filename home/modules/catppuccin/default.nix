07905b7b2bba44e8cb8a328938f5b1e8f7526247
unlsycn <unlsycn@unlsycn.com>
2026-02-16 22:34:30 +0800

    fix(vscode): sync icon theme and theme
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/catppuccin/default.nix
+++ home/modules/catppuccin/default.nix
@@ -1,11 +1,14 @@
-{ inputs, ... }:
+{ config, inputs, ... }:
 {
   imports = [ inputs.catppuccin.homeModules.catppuccin ];
 
   catppuccin = {
     flavor = "mocha";
     waybar.mode = "createLink";
-    vscode.profiles.default.flavor = "macchiato";
+    vscode.profiles.default = {
+      flavor = "macchiato";
+      icons.flavor = config.catppuccin.vscode.profiles.default.flavor;
+    };
     fcitx5.enable = false;
   };
 }
