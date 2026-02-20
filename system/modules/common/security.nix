3e71277fca07a660a12c2d45e5a6da5916262bca
unlsycn <unlsycn@unlsycn.com>
2026-02-21 00:55:19 +0800

    feat(security): auto-promote host ssh key to user age key
    Adds a systemd oneshot service to convert the host's SSH ed25519 key to an age key for user sops decryption, ensuring correct permissions.
    
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- system/modules/common/security.nix
+++ system/modules/common/security.nix
@@ -1,15 +1,51 @@
-{ ... }:
 {
-  security.sudo.enable = false;
-  security.doas = {
-    enable = true;
-    wheelNeedsPassword = true;
-    extraRules = [
-      {
-        groups = [ "wheel" ];
-        keepEnv = true;
-        persist = true;
-      }
-    ];
+  config,
+  user,
+  pkgs,
+  ...
+}:
+{
+  security = {
+    sudo.enable = false;
+    doas = {
+      enable = true;
+      wheelNeedsPassword = true;
+      extraRules = [
+        {
+          groups = [ "wheel" ];
+          keepEnv = true;
+          persist = true;
+        }
+      ];
+    };
+  };
+
+  systemd.services.promote-ssh-key-to-user-age = {
+    description = "Convert host SSH key to age key for user Home Manager";
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    script =
+      let
+        userAgeKeyFile = config.home-manager.users.${user}.sops.age.keyFile;
+      in
+      ''
+        mkdir -p "$(dirname "${userAgeKeyFile}")"
+        if [ -f /etc/ssh/ssh_host_ed25519_key ]; then
+          ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key > "${userAgeKeyFile}"
+          
+          chown ${user}:users "${userAgeKeyFile}"
+          chmod 600 "${userAgeKeyFile}"
+          
+          echo "Host SSH key has been promoted to ${userAgeKeyFile}"
+        else
+          echo "Host SSH key not found, skipping conversion."
+          exit 1
+        fi
+      '';
   };
 }
