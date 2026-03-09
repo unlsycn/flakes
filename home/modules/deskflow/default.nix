a7591f19adf3e7c2ae2bd726efc5f1d8be4e9da9
unlsycn <unlsycn@unlsycn.com>
2026-03-09 16:27:28 +0800

    refactor(deskflow): guard config with mkIf enable
    The deskflow server config was applied unconditionally despite the module defining an enable option. Wrap config in mkIf to prevent activation unless explicitly enabled.
    
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/deskflow/default.nix
+++ home/modules/deskflow/default.nix
@@ -1,35 +1,44 @@
-{ ... }:
-
+{
+  config,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.services.deskflow;
+in
 {
   imports = [ ./options.nix ];
 
-  services.deskflow = {
-    server = {
-      enable = true;
-      config = {
-        screens = {
-          allay = { };
-          artanis = { };
-        };
-
-        aliases = { };
-
-        options = {
-          switchDoubleTap = 250;
-        };
-
-        links = {
-          allay = {
-            left = [
-              {
-                screen = "artanis";
-                srcStart = 5;
-                srcEnd = 100;
-              }
-            ];
+  config = mkIf cfg.enable {
+    services.deskflow = {
+      server = {
+        enable = true;
+        config = {
+          screens = {
+            allay = { };
+            artanis = { };
           };
-          artanis = {
-            right = "allay";
+
+          aliases = { };
+
+          options = {
+            switchDoubleTap = 250;
+          };
+
+          links = {
+            allay = {
+              left = [
+                {
+                  screen = "artanis";
+                  srcStart = 5;
+                  srcEnd = 100;
+                }
+              ];
+            };
+            artanis = {
+              right = "allay";
+            };
           };
         };
       };
