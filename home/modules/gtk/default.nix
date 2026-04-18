d18d018edccf4d38e00eb3831632d870db62a54d
unlsycn <unlsycn@unlsycn.com>
2026-04-18 12:47:18 +0800

    fix(nix): align module config with upstream changes
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- home/modules/gtk/default.nix
+++ home/modules/gtk/default.nix
@@ -10,10 +10,13 @@ let
 in
 {
   config = mkIf cfg.enable {
-    gtk.cursorTheme = {
-      size = 32;
-      name = "BreezeX-RosePine-Linux";
-      package = pkgs.rose-pine-cursor;
+    gtk = {
+      cursorTheme = {
+        size = 32;
+        name = "BreezeX-RosePine-Linux";
+        package = pkgs.rose-pine-cursor;
+      };
+      gtk4.theme = config.gtk.theme;
     };
   };
 }
